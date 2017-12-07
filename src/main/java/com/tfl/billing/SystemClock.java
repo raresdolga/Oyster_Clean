f2ed66a4cb8f43e8a2bee15460b403f47a0fd10b
Andrei Margeloiu <andrei.margeloiu@gmail.com>
2017-12-07 16:55:43 +0000

    simplified SystemClock

--- src/main/java/com/tfl/billing/SystemClock.java
+++ src/main/java/com/tfl/billing/SystemClock.java
@@ -3,15 +3,8 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class SystemClock implements Clock {
-    private LocalDateTime now;
-    private long epoch;
-
     @Override
     public long now() {
-        now = LocalDateTime.now();
-        ZoneId zoneId = ZoneId.systemDefault();
-        //transform local time in milliseconds
-        epoch = now.atZone(zoneId).toInstant().toEpochMilli();
-        return  epoch;
+        return System.currentTimeMillis();
     }
 }
